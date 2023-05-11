9cc790fbf12e2f1e7ec325a799f1748210715d2f
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-11 16:57:45 +0200

    test ticket

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// Visualizzare in pagina 5 numeri casuali.
+let arrayNumber = randomOrderForRandomNumber ();
+console.log(arrayNumber);
+
+// Da lì parte un timer di 30 secondi.
+
+
+// Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// FUNCTION
+function randomNumber () {
+    let n = Math.floor((Math.random() * 100) + 1 );
+    return n 
+}
+function randomOrderForRandomNumber () {
+    let intArray = [] ;
+    for (let i = intArray.length; intArray.length < 5; i++) {
+        let n = randomNumber;
+        if (!intArray.includes(n)) {
+            intArray.push(n);
+        }
+        return intArray
+    }
+}
